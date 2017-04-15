e33eef73ab430a1a084bee3e7b63cfc9f104fbcf
Nirakar Mohanty <nirakarmohanty@gmail.com>
2017-04-16 00:09:58 +0530

    Spring Boot Application

---
+++ src/main/java/com/nirakar/boot/jdbc/Student.java
@@ -0,0 +1,29 @@
+package com.nirakar.boot.jdbc;
+
+public class Student {
+	private int id;
+	private String name;
+	private int age;
+
+	public Student(int id, String name, int age) {
+		this.id = id;
+		this.name = name;
+		this.age = age;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public String toString() {
+		return "Employee (" + id + ", " + name + ", " + age + ")";
+	}
+}
